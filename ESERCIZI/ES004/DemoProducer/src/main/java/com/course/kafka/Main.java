f63565a35e959531ff041eea53f5aec85709810c
aminelli <antonio.minelli@etlforma.com>
2025-11-17 10:59:27 +0100

    upd

--- ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/Main.java
+++ ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/Main.java
@@ -1,5 +1,7 @@
 package com.course.kafka;
 
+import com.course.kafka.producer.ProducerFireAndForget;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -10,6 +12,11 @@ public class Main {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+
+    public static void Menu01(){
+        var producer = new ProducerFireAndForget();
+        producer.sendMessages("CORSO_FAF", 5000);
     }
 
     public static void main(String[] args) {
@@ -26,16 +33,18 @@ public class Main {
             System.out.println("MENU PRINCIPALE");
             System.out.println("-".repeat(30));
             System.out.println("1. Producer - Caso 1 - Fire And Forget (ack 0)");
-            System.out.println("0. Esci");
-            System.out.println("\n");
-            System.out.println("\n");
             System.out.println("-".repeat(30));
+            System.out.println("0. Esci");
+            System.out.println("-".repeat(30));
+            System.out.println("\n");
+            System.out.println("\n");
             System.out.println("Scegli un'opzione: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("\n");
+                    //System.out.println("\n");
+                    Menu01();
                     break;
                 case 0:
                     System.out.println("Ciao");
--- ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/producer/ProducerFireAndForget.java
+++ ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/producer/ProducerFireAndForget.java
@@ -1,8 +1,32 @@
 package com.course.kafka.producer;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
 public class ProducerFireAndForget {
 
     public void sendMessages(String topicName, long totalMessages) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Properties props = new Properties();
+
+        // Endpoints dei nodi del cluster
+        //props.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092, localhost:9093, localhost:9094");
+
+        // Client ID
+        props.put(ProducerConfig.CLIENT_ID_CONFIG,"PRD-001");
+
+        // Algoritmo di compressione
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG,"gzip");
+
+        // TIPO ACK
+        props.put(ProducerConfig.ACKS_CONFIG, "0");
+
 
     }
 

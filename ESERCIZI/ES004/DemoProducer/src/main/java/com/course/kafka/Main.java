b1ebc69b73a1e03e201c7e349c770b402bcf38a4
aminelli <antonio.minelli@etlforma.com>
2025-11-17 10:17:11 +0100

    upd

--- ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/Main.java
+++ ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/Main.java
@@ -1,17 +1,51 @@
 package com.course.kafka;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+
+        do {
+            // Print menu
+            clearScreen();
+
+            System.out.println("-".repeat(30));
+            System.out.println("MENU PRINCIPALE");
+            System.out.println("-".repeat(30));
+            System.out.println("1. Producer - Caso 1 - Fire And Forget (ack 0)");
+            System.out.println("0. Esci");
+            System.out.println("\n");
+            System.out.println("\n");
+            System.out.println("-".repeat(30));
+            System.out.println("Scegli un'opzione: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("\n");
+                    break;
+                case 0:
+                    System.out.println("Ciao");
+                    break;
+                default:
+                    System.out.println("Opzione non valida !!!");
+            }
+
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
---
+++ ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/producer/ProducerFireAndForget.java
@@ -0,0 +1,9 @@
+package com.course.kafka.producer;
+
+public class ProducerFireAndForget {
+
+    public void sendMessages(String topicName, long totalMessages) {
+
+    }
+
+}

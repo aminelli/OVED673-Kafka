3150e61b1d99ad84c8980ae156eb58b4047935df
aminelli <antonio.minelli@etlforma.com>
2025-11-17 12:34:45 +0100

    upd

---
+++ ESERCIZI/ES002/script-02-start-alternativo.ps1
@@ -0,0 +1,10 @@
+
+
+docker pull apache/kafka:4.1.0
+
+# Creo una rete docker di tipo bridge dedicata alle instanze kafka
+docker network create net-kafka
+
+# Creo un'istanza kafka
+docker run -d --name broker-kafka --hostname broker-kafka --network net-kafka -p 9092:9092 --env-file .env.broker apache/kafka:4.1.0
+

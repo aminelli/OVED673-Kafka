a1b3c7df58de5f6438e29165ede008e1afec6f2e
aminelli <antonio.minelli@etlforma.com>
2025-11-17 17:14:25 +0100

    upd

--- ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/partiotioner/CustomPartitioner.java
+++ ESERCIZI/ES004/DemoProducer/src/main/java/com/course/kafka/partiotioner/CustomPartitioner.java
@@ -6,15 +6,17 @@ import org.apache.kafka.common.Cluster;
 import java.util.Map;
 
 public class CustomPartitioner implements Partitioner {
+
+
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
 
         if (key != null && key instanceof String) {
 
             String keyString = (String) key;
-            if (keyString.startsWith("K")) {
+            if (keyString.startsWith("JSON")) {
                 return 0;
-            } else if (keyString.startsWith("Q")) {
+            } else if (keyString.startsWith("XML")) {
                 return 1;
             } else {
                 // CASO X
@@ -26,11 +28,11 @@ public class CustomPartitioner implements Partitioner {
 
     @Override
     public void close() {
-
+        System.out.println("Closing Partitioner");
     }
 
     @Override
     public void configure(Map<String, ?> map) {
-
+        System.out.println("Configuring Partitioner");
     }
 }

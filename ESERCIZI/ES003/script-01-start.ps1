a4dda91de86d52a483e1d31b92feac9d36f06731
aminelli <antonio.minelli@etlforma.com>
2025-11-17 09:10:53 +0100

    upd

---
+++ ESERCIZI/ES003/script-01-start.ps1
@@ -0,0 +1,19 @@
+
+
+docker pull confluentinc/cp-server:7.9.4 
+docker pull confluentinc/cp-enterprise-control-center:7.9.4
+
+docker network create net-kafka
+
+docker volume create vol-broker01
+docker volume create vol-broker02
+docker volume create vol-broker03
+
+docker run -d --hostname broker01 --name broker01 --network net-kafka -p 9092:9092 -p 9101:9101 --env-file .env.broker01 -v vol-broker01:/klogs/kraft-combined-logs confluentinc/cp-server:7.9.4
+docker run -d --hostname broker02 --name broker02 --network net-kafka -p 9093:9093 -p 9102:9101 --env-file .env.broker02 -v vol-broker02:/klogs/kraft-combined-logs confluentinc/cp-server:7.9.4
+docker run -d --hostname broker03 --name broker03 --network net-kafka -p 9094:9094 -p 9103:9101 --env-file .env.broker03 -v vol-broker03:/klogs/kraft-combined-logs confluentinc/cp-server:7.9.4
+
+docker run -d --hostname kafka-ui --name kafka-ui -p 9088:8080 -e DYNAMIC_CONFIG_ENABLED=true --network net-kafka provectuslabs/kafka-ui
+
+docker run -d --hostname center --name center -p 9021:9021 --network net-kafka --env-file .env.control-center confluentinc/cp-enterprise-control-center:7.9.4
+

aeb09de024fb15c087d2d44a08cd2aa8510d2bd5
aminelli <antonio.minelli@etlforma.com>
2025-11-24 17:49:52 +0100

    upd

--- ESERCIZI/ES007/01-create-container.bat
+++ ESERCIZI/ES007/01-create-container.bat
@@ -7,10 +7,23 @@ docker run -d --name mongo --hostname mongo --network kafka-cluster_net-kafka-cl
 
 docker run -d --name elastic --hostname elastic --network kafka-cluster_net-kafka-cluster -p 9200:9200 -p 9300:9300 -e discovery.type=single-node elasticsearch:7.6.2
 
+
 docker exec -it -u root ksqldb-server /bin/bash
 dnf update -y
 dnf install nano -y
+cd /usr/share
+mkdir kafka
 
 nano /etc/environment
+KSQL_CONNECT_PLUGIN_PATH=/usr/share/kafka/confluent-hub-components
+KSQL_CONNECT_CONFIG_STORAGE_TOPIC=_ksql-connect-configs
+KSQL_CONNECT_OFFSET_STORAGE_TOPIC=_ksql-connect-offsets
+KSQL_CONNECT_STATUS_STORAGE_TOPIC=_ksql-connect-statuses
+KSQL_CONNECT_GROUP_ID=ksql-connect-cluster
+KSQL_CONNECT_BOOTSTRAP_SERVERS=broker:9092
+KSQL_CONNECT_KEY_CONVERTER=org.apache.kafka.connect.storage.StringConverter
+KSQL_CONNECT_VALUE_CONVERTER=io.confluent.connect.avro.AvroConverter
+KSQL_CONNECT_VALUE_CONVERTER_SCHEMA_REGISTRY_URL=http://schema-registry:8081
 
-docker cp 
+
+docker cp confluent-hub-components ksqldb-server:/usr/share/kafka

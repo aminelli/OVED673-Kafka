372028459bcfb5e4c591a8c58162e1bc5175c8d6
aminelli <antonio.minelli@etlforma.com>
2025-11-24 17:08:04 +0100

    upd

--- ESERCIZI/ES007/01-create-container.bat
+++ ESERCIZI/ES007/01-create-container.bat
@@ -4,3 +4,5 @@
 docker run -d --name postgres --hostname postgre --network kafka-cluster_net-kafka-cluster -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=customers -v ./postgres/custom-config.conf:/etc/postgresql/postgresql.conf debezium/postgres:12 postgres -c config_file=/etc/postgresql/postgresql.conf
 
 docker run -d --name mongo --hostname mongo --network kafka-cluster_net-kafka-cluster -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_REPLICA_SET_NAME=my-replica-set mongo:4.2.5
+
+docker run -d --name elastic --hostname elastic --network kafka-cluster_net-kafka-cluster -p 9200:9200 -p 9300:9300 -e discovery.type=single-node elasticsearch:7.6.2

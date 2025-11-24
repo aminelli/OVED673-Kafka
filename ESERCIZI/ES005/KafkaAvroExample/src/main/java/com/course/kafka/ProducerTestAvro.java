d479ecc6815e27dba32f934780d5589dfb2f1302
aminelli <antonio.minelli@etlforma.com>
2025-11-24 10:57:22 +0100

    upd

--- ESERCIZI/ES005/KafkaAvroExample/src/main/java/com/course/kafka/ProducerTestAvro.java
+++ ESERCIZI/ES005/KafkaAvroExample/src/main/java/com/course/kafka/ProducerTestAvro.java
@@ -1,8 +1,14 @@
 package com.course.kafka;
 
+import com.course.kafka.model.avro.Payment;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import net.datafaker.Faker;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.InterruptException;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -12,7 +18,9 @@ public class ProducerTestAvro {
     private static final String TOPIC = "TEST_AVRO";
     private static final Properties props = new Properties();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Faker faker = new Faker();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
@@ -22,6 +30,28 @@ public class ProducerTestAvro {
 
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+
+        try (KafkaProducer<String, Payment> producer = new KafkaProducer<String, Payment>(props)) {
+
+            for (long count = 0; count < 1000; count++) {
+                final Integer id = faker.idNumber().hashCode();
+                final Double amount = faker.number().numberBetween(100d, 10000d);
+
+                final Payment payment = new Payment(id.toString(), amount);
+                final ProducerRecord<String, Payment> record = new ProducerRecord<>(TOPIC, id.toString(), payment);
+                producer.send(record);
+                Thread.sleep(10L);
+
+            }
+
+            producer.flush();
+            producer.close();
+
+        } catch (final SerializationException ex) {
+            ex.printStackTrace();
+        } catch (final InterruptException ex) {
+            ex.printStackTrace();
+        }
 
     }
 

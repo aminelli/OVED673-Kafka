bb171cce4144f22515909deeb5619f15af45ec78
aminelli <antonio.minelli@etlforma.com>
2025-11-24 17:40:20 +0100

    upd

--- ESERCIZI/ES007/01-create-container.bat
+++ ESERCIZI/ES007/01-create-container.bat
@@ -6,3 +6,11 @@ docker run -d --name postgres --hostname postgre --network kafka-cluster_net-kaf
 docker run -d --name mongo --hostname mongo --network kafka-cluster_net-kafka-cluster -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -e MONGO_REPLICA_SET_NAME=my-replica-set mongo:4.2.5
 
 docker run -d --name elastic --hostname elastic --network kafka-cluster_net-kafka-cluster -p 9200:9200 -p 9300:9300 -e discovery.type=single-node elasticsearch:7.6.2
+
+docker exec -it -u root ksqldb-server /bin/bash
+dnf update -y
+dnf install nano -y
+
+nano /etc/environment
+
+docker cp 

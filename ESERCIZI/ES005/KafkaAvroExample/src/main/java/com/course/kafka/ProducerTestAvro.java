2015ed35d69bff8e62a5ff6454c72d7685b03915
aminelli <antonio.minelli@etlforma.com>
2025-11-24 10:38:23 +0100

    upd

--- ESERCIZI/ES005/KafkaAvroExample/src/main/java/com/course/kafka/ProducerTestAvro.java
+++ ESERCIZI/ES005/KafkaAvroExample/src/main/java/com/course/kafka/ProducerTestAvro.java
@@ -1,7 +1,28 @@
 package com.course.kafka;
 
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerTestAvro {
 
-    public static void main(String[] args) {}
-    
+    private static final String TOPIC = "TEST_AVRO";
+    private static final Properties props = new Properties();
+
+    public static void main(String[] args) {
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+
+    }
+
 }
